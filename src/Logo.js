923789e3585720d123c00384a55f5090fe4342ea
Daniel Pittman <dpdanpittman@gmail.com>
2023-10-23 13:56:34 -0600

    update

---
+++ src/Logo.js
@@ -0,0 +1,9 @@
+// Logo.js
+import React from 'react';
+import logo from './logo.png'; // Replace with the path to your logo image
+
+function Logo() {
+  return <img src={logo} alt="Logo" className="logo" />;
+}
+
+export default Logo;
