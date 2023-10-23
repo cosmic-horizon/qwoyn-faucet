b8fc9f852e885ccddb3ebd5d7841e4e962746c22
Daniel Pittman <dpdanpittman@gmail.com>
2023-10-23 11:01:45 -0600

    update faucet

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ const App = () => {
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
-  const is_valid_qwoyn_address = (address) => {
-    if (address.length !== 43 || !address.startsWith('qwoyn')) {
+  const is_valid_osmosis_address = (address) => {
+    if (address.length !== 43 || !address.startsWith('osmo')) {
       return false;
     }
 
@@ -20,47 +20,52 @@ const App = () => {
     return true;
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     setSuccessMessage('');
     setErrorMessage('');
 
-    if (!is_valid_qwoyn_address(address)) {
-      setErrorMessage('Invalid Qwoyn address');
+    if (!is_valid_osmosis_address(address)) {
+      setErrorMessage('Invalid Osmosis address');
       return;
     }
 
-    // Simulate an AJAX request here (replace with your actual AJAX code)
-    setTimeout(() => {
-      // Simulate a successful response
-      setSuccessMessage('Tokens requested successfully');
-    }, 1000);
+    try {
+      // Send a request to the specified URL and fetch the address data
+      const response = await fetch(`http://45.77.214.92:5000/?address=${address}`);
+      if (response.ok) {
+        const data = await response.json();
+        setSuccessMessage(data);
+      } else {
+        throw new Error('Request failed');
+      }
+    } catch (error) {
+      setErrorMessage('Failed to request tokens');
+    }
   };
 
   return (
-    <div className="container mt-5">
-      <div className={`alert alert-success ${successMessage ? '' : 'd-none'}`}>
-        {successMessage}
+      <div className="container mt-5">
+        <div className={`alert alert-success ${successMessage ? '' : 'd-none'}`}>{successMessage}</div>
+        <div className={`alert alert-danger ${errorMessage ? '' : 'd-none'}`}>{errorMessage}</div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="address-input">Enter Osmosis Address:</label>
+            <input
+                type="text"
+                className="form-control"
+                id="address-input"
+                required
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Request Tokens
+          </button>
+        </form>
       </div>
-      <div className={`alert alert-danger ${errorMessage ? '' : 'd-none'}`}>{errorMessage}</div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="address-input">Enter Qwoyn Address:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="address-input"
-            required
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Request Tokens
-        </button>
-      </form>
-    </div>
   );
 };
 
