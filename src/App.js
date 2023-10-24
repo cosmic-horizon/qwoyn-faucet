681515269fe147cf30e1b980e27663c2d7b581c2
Daniel Pittman <dpdanpittman@gmail.com>
2023-10-24 11:05:46 -0600

    spruce up button interaction

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ const App = () => {
         <div className="container">
           <div className="description-container">
             <p>
-              Welcome to our Qwoyn Faucet! Please enter your Qwoyn address below, and 10 $QWOYN will be sent to your wallet.
-              This will only work once! If you need more tokens, please contact our dev team.
+              Welcome to the Qwoyn Testnet Faucet! Please enter your Qwoyn address below, and 10 $QWOYN will be sent to your wallet.
+              This will only work once! If you need more tokens, please contact our dev team via Discord.
             </p>
           </div>
 
--- src/Description.js
+++ src/Description.js
@@ -4,8 +4,8 @@ const Description = () => {
     return (
         <div className="description-container">
             <p>
-                Welcome to our Qwoyn Faucet! Please enter your Qwoyn address below, and 10 $QWOYN will be sent to your wallet.
-                This will only work once! If you need more tokens, please contact our dev team.
+                Welcome to the Qwoyn TestnetFaucet! Please enter your Qwoyn address below, and 10 $QWOYN will be sent to your wallet.
+                This will only work once! If you need more tokens, please contact our dev team on Discord.
             </p>
         </div>
     );
