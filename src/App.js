ebbb6e13681ec45e87aca2213e7cb675f4eff35e
Daniel Pittman <dpdanpittman@gmail.com>
2023-10-24 10:46:59 -0600

    update module

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Header from './Header';
 import Form from './Form'; // Import the Form component
 
 const App = () => {
-  const [successMessage, setSuccessMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  // const [successMessage, setSuccessMessage] = useState('');
+  // const [errorMessage, setErrorMessage] = useState('');
 
   const handleFormSubmit = (formData) => {
     // You can handle the form submission data here
     console.log('Form data submitted:', formData);
 
     // Set the success message or error message based on your actual API response
-    setSuccessMessage('Tokens sent successfully'); // Replace with your success message
+    // setSuccessMessage('Tokens sent successfully'); // Replace with your success message
     // setErrorMessage('Transaction failed'); // Replace with your error message
   };
 
@@ -28,16 +28,7 @@ const App = () => {
               This will only work once! If you need more tokens, please contact our dev team.
             </p>
           </div>
-          {successMessage && (
-              <div className="alert alert-success" style={{ color: 'green' }}>
-                {successMessage}
-              </div>
-          )}
-          {errorMessage && (
-              <div className="alert alert-danger">
-                {errorMessage}
-              </div>
-          )}
+
           <Form onSubmit={handleFormSubmit} />
         </div>
       </div>
